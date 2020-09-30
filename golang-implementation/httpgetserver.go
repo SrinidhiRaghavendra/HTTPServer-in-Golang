daa234c6d563ad5180dc00b6eef99155fe4c358e
SrinidhiRaghavendra <sraghav2@cs.binghamton.edu>
2020-09-29 22:40:28 -0400

    Adding the following feature requirements:\n1. Added a struct to maintain a map[string]int for each filename to store the number of accesses.\n2. Synchronised the access to the map using a mutex in the same struct.\n3. Added another mutex to synchronise the print of logs to stdout.\n4. Handled the error cases of www directory not present and the requested resource not present\nTODO: 1. Create a HTTP response for each hit request.\n2. Send the HTTP repsonse header as well as data as TCP response.\n3. Finally close the connection

--- golang-implementation/httpgetserver.go
+++ golang-implementation/httpgetserver.go
@@ -6,7 +6,18 @@ import (
   "strings"
   "net"
   "os"
+  "bufio"
+  "sync"
+  "strconv"
 )
+
+type AccessCounter struct {
+  counts map[string]int
+  mux sync.Mutex
+}
+
+var accessCounter = AccessCounter{counts: make(map[string]int)}
+var printMutex sync.Mutex
 
 func main() {
   data, err := ioutil.ReadFile("/etc/hostname")
@@ -37,6 +48,54 @@ func main() {
 }
 
 func handleConnection(conn net.Conn) {
-  fmt.Printf("The address of the client is as follows: ", string(conn.RemoteAddr().String()))
-  fmt.Println(conn)
+  clientAddr := string(conn.RemoteAddr().String())
+  reader := bufio.NewReader(conn)
+  var data string
+  var err error
+  for i := 0; i < 2; i++{ //Because the second word points to the resource requested
+    data, err = reader.ReadString(' ');
+    data = strings.TrimSpace(data)
+    if err != nil {
+      log.Print("Error in HTTP header, no resource specified")
+      conn.Close()
+      return
+    }
+  }
+  filename := "www"+data
+  count := accessCounter.updateCount(filename)
+  log.Print("Access count: ", count)
+  response := ""
+  log.Print("Searching for Requested resource: ", data)
+  if _, err := os.Stat(filename); err != nil {
+        if os.IsNotExist(err) {
+	  response = "404 Not Found"
+	} else {
+	  response = "200 OK"
+	}
+  }
+  log.Print(response)
+  // TODO: build HTTP repsonse 
+  // logging the required output in the required format
+  printLog(clientAddr, data[1:], count)
 }
+
+func printLog(clientAddr string, resource string, count int) {
+  printMutex.Lock()
+  defer printMutex.Unlock()
+  clientAddrParts := strings.Split(clientAddr, ":")
+  log.Print(resource + "|" + clientAddrParts[0] + "|" + clientAddrParts[1] + "|" + strconv.Itoa(count))
+}
+
+func (accessCounter *AccessCounter) updateCount(filename string) int {
+  accessCounter.mux.Lock()
+  defer accessCounter.mux.Unlock()
+  value, ok := accessCounter.counts[filename]
+  if(ok) {
+    accessCounter.counts[filename] = value + 1
+  }else {
+    accessCounter.counts[filename] = 1
+  }
+  return accessCounter.counts[filename]
+}
+
+
